d7110b7cb7e21eefb2878210a5a828b0a5a6b240
sonp1998 <sonp1998@gmail.com>
2022-09-16 18:32:37 -0400

    Enhanced Card implementation, started Deck.

--- src/main/java/com/tlglearning/cards/model/Card.java
+++ src/main/java/com/tlglearning/cards/model/Card.java
@@ -1,11 +1,15 @@
 package com.tlglearning.cards.model;
+
+import java.util.Objects;
+
 //Object is the superclass automatically
-public class Card {
+public class Card implements Comparable<Card> {
+
   //1. field Declaration. modifier modifier Type Name. Final is asking the compiler to only have single value,
   private final Rank rank; // and in return expects initialization...
   private final Suit suit;
 
-//2. CONSTRUCTOR(      parameter     )
+  //2. CONSTRUCTOR(      parameter     )
   public Card(Rank rank, Suit suit) {
     this.rank = rank;
     this.suit = suit;
@@ -15,14 +19,37 @@ public class Card {
   public Rank rank() {
     return rank;
   }
+
   public Suit suit() {
     return suit;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(rank, suit); //FIXME Move this to initialization . (:
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result;
+    if (this == obj) {
+      result = true;
+    } else if (obj instanceof Card) {
+      Card other = (Card) obj;
+      result = (this.rank == other.rank && this.suit == other.suit);
+    } else {
+      result = false;
+    }
+    return result;
   }
 
   @Override
   public String toString() {
     return rank.symbol() + suit.symbol(); //string concatenation
   }
-  //TODO Override equals method.
-  //TODO Override hashcode method.
+
+  @Override
+  public int compareTo(Card other) {
+    return 0; //TODO replace with comparison based on suit and rank
+  }
 }
