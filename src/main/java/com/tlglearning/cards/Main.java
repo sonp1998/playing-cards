ce3bfefb44117b86c65b7809945089b4fd4ac3a9
sonp1998 <sonp1998@gmail.com>
2022-09-28 12:18:26 -0400

    Lambda inside main.

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -11,32 +11,23 @@ public class Main {
   public static void main(String[] args) {
     //constructing an anomalous class and instructing the instance into local variable comparator.
 
-    //create an instance of Deck
-    Deck deck = new Deck();
-    //print the string representation of the deck
-    System.out.println(deck);
-    //create an acceptable source of randomness. Cryptographically secure
-    Random rng = new SecureRandom();
-    //shuffle the deck using the created source of randomness
-    deck.shuffle(rng);
-    //Print the string representation of the deck after shuffling
-    System.out.println(deck);
+
+    Deck deck = new Deck();               //create an instance of Deck
+    System.out.println(deck);             //print the string representation of the deck
+    Random rng = new SecureRandom();     //create an acceptable source of randomness. Cryptographically secure
+    deck.shuffle(rng);                  //shuffle the deck using the created source of randomness
+    System.out.println(deck);           //Print the string representation of the deck after shuffling
     deck.sort();
     System.out.println(deck);
-    deck.sort(new Comparator<>() {
-      //anon class inside a method
-      @Override
-      public int compare(Card card1, Card card2) {
-        int comparison = card1.suit().color().compareTo(card2.suit().color());
-        if (comparison == 0) {
-          comparison = card1.suit().compareTo(card2.suit());
-          if (comparison==0) {
-            comparison = - card1.rank().compareTo(card2.rank()); // negative reverses the comparison going in descending order.
-          }
+    deck.sort((card1, card2) -> {
+      int comparison = card1.suit().color().compareTo(card2.suit().color());
+      if (comparison == 0) {
+        comparison = card1.suit().compareTo(card2.suit());
+        if (comparison==0) {
+          comparison = - card1.rank().compareTo(card2.rank()); // negative reverses the comparison going in descending order.
         }
-        return comparison;
       }
-
+      return comparison;
     });
     //creating an instance of 3rd party that does it in descending order.
     System.out.println(deck);
